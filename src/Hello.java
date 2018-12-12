820e66a2c33a4be1f161cabed3f39f5f38ab899d
praveenSelenium <praveen.mamidi@styleresearch.com>
2018-12-12 17:55:12 +0000

    testing

--- src/Hello.java
+++ src/Hello.java
@@ -3,9 +3,7 @@ public class Hello {
     public static void main(String[] args){
 
 
-        System.out.println("Praveen Test");
-        
-        System.out.println("Praveen Tfgfgfest");
+        System.out.println("Praveen Test");        
 
 
     }
